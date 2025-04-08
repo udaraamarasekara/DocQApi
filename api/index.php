e5c4e9292e4c9ebf122a7e44fcbd9e012f92c578
udara amarasekara <udaraprabath98@gmail.com>
2025-04-08 13:02:44 +0530

    host4

--- api/index.php
+++ api/index.php
@@ -1,3 +1,14 @@
-<?php 
+<?php
 
-require __DIR__ . "/../public/index.php";
+require 'vendor/autoload.php';
+
+$app = require_once 'bootstrap/app.php';
+
+// Use Laravel's routing
+$response = $app->make('Illuminate\Contracts\Http\Kernel')->handle(
+    Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$app->terminate($request, $response);
